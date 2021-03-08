f345b6894dce20b104202cdb7cce4586d8948cc0
Gabriel Lopes <voltsggl@gmail.com>
2021-03-08 15:56:37 -0300

    -

---
+++ auladejava.js
@@ -0,0 +1,13 @@
+const subtrai - function(a,b){
+    return console.log("resultado da subtração:"), a - b;
+  }
+  let resultado - subtrai(3,2);
+  console.log(resultado);
+  
+  const testaZero = (a)=>{
+    return a ? "NÃO É ZERO" : "É ZERO";
+  }
+  let resultado2 = testaZero(1);
+  console.log(resultado2);
+  
+  console.log("FIM");
