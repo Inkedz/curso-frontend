4088aa15ca66d28af041da73841a8bae9b55b6e3
Gabriel Lopes <voltsggl@gmail.com>
2021-03-08 16:45:49 -0300

    Aula de Java desafio aula10

--- auladejava.js
+++ auladejava.js
@@ -1,13 +1,28 @@
-const subtrai - function(a,b){
-    return console.log("resultado da subtração:"), a - b;
+const subtrai = function (a, b) {
+  return console.log("resultado da subtração:"), a - b;
+};
+let resultado = subtrai(3, 2);
+console.log(resultado);
+
+const testaZero = (a) => {
+  return a ? "NÃO É ZERO" : "É ZERO";
+};
+let resultado2 = testaZero(1);
+console.log(resultado2);
+
+function testaSinal(a, b) {
+  const func = function (a, b) {
+    return subtrai(a, b);
   }
-  let resultado - subtrai(3,2);
-  console.log(resultado);
-  
-  const testaZero = (a)=>{
-    return a ? "NÃO É ZERO" : "É ZERO";
+  let resultado3 = func(a, b);
+
+  if (testaZero(resultado3) == "É ZERO") {
+    console.log("É ZERO");
+  } else if ((a < 0 && b < 0) || (a > 0 && b > 0)) {
+    console.log("MESMO SINAL");
+  } else {
+    console.log("SINAL DIFERENTE");
   }
-  let resultado2 = testaZero(1);
-  console.log(resultado2);
-  
-  console.log("FIM");
+}
+testaSinal(-1, 4);
+console.log("FIM");
